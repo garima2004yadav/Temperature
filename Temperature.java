db03bef030dcfe3e97875f7e043db777a601e307
garima2004yadav <115277218+garima2004yadav@users.noreply.github.com>
2023-10-17 22:43:15 +0530

    Add files via upload

---
+++ Temperature.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(system.in);
+        System.out.println("Enter temperature");
+        float temperature = sc.nextfloat();
+        float fh = (F-32)* 5 / 9;
+        
+
+    
+    }
+    
+}
